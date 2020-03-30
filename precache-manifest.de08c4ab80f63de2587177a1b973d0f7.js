2e54cc047c2441325164d39829d3c1ed58365641
park singyu <psk876@naver.com>
2020-03-30 15:57:47 +0900

    Updates

---
+++ precache-manifest.de08c4ab80f63de2587177a1b973d0f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44524e0fe849fa3c8273ca970f9536d0",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "cbb5a2a6fe72460dc2a9",
+    "url": "/movie_app/static/css/main.247ade7a.chunk.css"
+  },
+  {
+    "revision": "6749bf3d58a126aac959",
+    "url": "/movie_app/static/js/2.fa804b4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.fa804b4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb5a2a6fe72460dc2a9",
+    "url": "/movie_app/static/js/main.5ed662eb.chunk.js"
+  },
+  {
+    "revision": "5d53b70c652fc6a3e5ec",
+    "url": "/movie_app/static/js/runtime-main.11e1f2ff.js"
+  }
+]);
